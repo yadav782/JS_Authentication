1f81639a9b3cd6a3a5fc75f67313fa996b9be6b5
Yadav Prakash <yadav.782@gmail.com>
2021-09-10 14:07:13 +0200

    bcrypt code with hash and saltrounds

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express")
 const ejs = require("ejs")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
-const encrypt = require('mongoose-encryption');
+// const encrypt = require('mongoose-encryption'); (Step 1 Auth)
+const bcrypt = require("bcrypt")
+const saltRounds = 10;
 
 const app = express();
 
@@ -23,53 +25,66 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password'] });
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password'] }); (Step 1 auth)
 
 const User = new mongoose.model("User", userSchema)
 
-app.get("/", function(req,res){
+app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/login", function(req,res){
+app.get("/login", function(req, res) {
   res.render("login");
 });
 
-app.get("/register", function(req,res){
+app.get("/register", function(req, res) {
   res.render("register");
 });
 
-app.post("/register", function(req,res){
-  const newUser = new User({
-    email : req.body.username,
-    password : req.body.password
-  });
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    } else {
-      res.render("secrets")
-    }
-  });
+app.post("/register", function(req, res) {
 
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets")
+      }
+    });
+
+  });
 });
 
-app.post("/login", function(req,res){
+
+
+app.post("/login", function(req, res) {
   const userName = req.body.username;
   const userPassword = req.body.password;
 
-  User.findOne({email:userName}, function(err, foundEmail){
-    if(err){
+  User.findOne({
+    email: userName
+  }, function(err, foundEmail) {
+    if (err) {
       console.log(err);
-    } else{
-      if(foundEmail.password === userPassword){
-        res.render("secrets")
+    } else {
+      if (foundEmail) {
+        bcrypt.compare(userPassword, foundEmail.password, function(err, result) {
+          if(result===true){
+            res.render("secrets")
+          }
+
+        });
       }
     }
   })
 })
 
 
-app.listen("3000",function(req,res){
+app.listen("3000", function(req, res) {
   console.log("Server is running on port 3000");
 })
